6967e15e21704037490df3128e2814f59c7f0b63
Kyle Jensen <kjensen19@gmail.com>
2022-12-29 20:36:58 -0600

    add polls models and enable via settings.py

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# Each model is a subclass of django.db.models.Model
+# Left side represents field and db column name
+# First arg to the models.xxx() is if a human readable name is needed (ex. date published)-- optional
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+# Each choice is tied to a question via fKey
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
