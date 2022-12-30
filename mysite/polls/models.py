ecc2c4445c7a61f1759213560d22f3e32a52fa6f
Kyle Jensen <kjensen19@gmail.com>
2022-12-29 22:38:13 -0600

    add admin view, finish model modifications

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,6 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Each model is a subclass of django.db.models.Model
 # Left side represents field and db column name
@@ -7,6 +9,10 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 # Each choice is tied to a question via fKey
 
@@ -14,3 +20,5 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
