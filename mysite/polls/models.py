dc7f0b8afd0e0d369dc72a46e88d80dff37f8b12
Kyle Jensen <kjensen19@gmail.com>
2022-12-31 23:11:31 -0600

    styling to admin views

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 # Each model is a subclass of django.db.models.Model
 # Left side represents field and db column name
@@ -11,6 +12,12 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
+
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
